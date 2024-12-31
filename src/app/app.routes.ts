c7ac40066bd60d67e5d8fe91e59a5d96a0294100
Regina Gaigl <regina.gaigl@gmail.com>
2024-12-31 11:56:11 +0100

    font

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './main-content/home/home.component';
 import { LegalNoticeComponent } from './info-content/legal-notice/legal-notice.component';
 import { PrivacyPolicyComponent } from './info-content/privacy-policy/privacy-policy.component';
+import { MainContentComponent } from './main-content/main-content/main-content.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', component: MainContentComponent },
     { path: 'legal-notice', component: LegalNoticeComponent },
     { path: 'privacy-policy', component: PrivacyPolicyComponent }
 ];
---
+++ src/app/main-content/main-content/main-content.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { WhyMeComponent } from '../why-me/why-me.component';
+import { SkillsComponent } from '../skills/skills.component';
+import { MyWorkComponent } from '../my-work/my-work.component';
+import { ContactMeComponent } from '../contact-me/contact-me.component';
+
+@Component({
+  selector: 'app-main-content',
+  standalone: true,
+  imports: [
+    HomeComponent,
+    WhyMeComponent,
+    SkillsComponent,
+    MyWorkComponent,
+    ContactMeComponent
+  ],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss'
+})
+export class MainContentComponent {
+
+}
