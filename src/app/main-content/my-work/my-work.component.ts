030c3e088ff7341398f15a561818676245efd3f3
Regina Gaigl <regina.gaigl@gmail.com>
2025-01-02 15:51:02 +0100

    basic project card

--- src/app/main-content/my-work/my-work.component.ts
+++ src/app/main-content/my-work/my-work.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
+import { ProjectCardComponent } from '../../info-project/project-card/project-card.component';
 
 @Component({
   selector: 'app-my-work',
   standalone: true,
-  imports: [TranslocoModule],
+  imports: [TranslocoModule, ProjectCardComponent ],
   templateUrl: './my-work.component.html',
   styleUrl: './my-work.component.scss'
 })
