0c34839946d0c19ad8e9fd3acb9f85aaf85c310c
Regina Gaigl <regina.gaigl@gmail.com>
2025-01-02 14:56:29 +0100

    my skills section

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
@@ -9,5 +9,21 @@ import { TranslocoModule } from '@jsverse/transloco';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+
+  icons = [
+    { 'img': 'angular.png', 'title': 'Angular' },
+    { 'img': 'typescript.png', 'title': 'TypeScript' },
+    { 'img': 'javascript.png', 'title': 'JavaScript' },
+    { 'img': 'html.png', 'title': 'HTML' },
+    { 'img': 'css.png', 'title': 'CSS' },
+    { 'img': 'rest-api.png', 'title': 'Rest-Api' },
+    { 'img': 'firebase.png', 'title': 'Firebase' },
+    { 'img': 'git.png', 'title': 'GIT' },
+    { 'img': 'scrum.png', 'title': 'Scrum' },
+    { 'img': 'material-design.png', 'title': 'Material Design' },
+    { 'img': 'bootstrap.png', 'title': 'Bootstrap' },
+    { 'img': 'copilot.png', 'title': 'Github Copilot' },
+    { 'img': 'challenge.png', 'title': 'Challange me' },
+  ];
 
 }
