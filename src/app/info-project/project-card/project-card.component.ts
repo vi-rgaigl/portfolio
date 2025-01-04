71fd3a67523238a417b7b8016baae33958cf1594
Regina Gaigl <regina.gaigl@gmail.com>
2025-01-04 09:34:30 +0100

    project card mobile

--- src/app/info-project/project-card/project-card.component.ts
+++ src/app/info-project/project-card/project-card.component.ts
@@ -1,14 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input} from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
-  imports: [TranslocoModule],
+  imports: [CommonModule, TranslocoModule],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })
 export class ProjectCardComponent {
 
   @Input() project: any;
+
+  isExpanded = false;
+
+  toggleExpand() {
+    this.isExpanded = !this.isExpanded;
+  }
 }
