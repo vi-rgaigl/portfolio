2cefe14962e0fe7602e06fbd1848e82677a4fd2f
Regina Gaigl <regina.gaigl@gmail.com>
2025-01-13 16:15:57 +0100

    imprint & privacy

--- src/app/info-content/legal-notice/legal-notice.component.ts
+++ src/app/info-content/legal-notice/legal-notice.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [TranslocoModule],
+  imports: [TranslocoModule , FooterComponent],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
+
+  closeTab() {
+    window.close();
+  }
 
 }
--- src/app/info-content/privacy-policy/privacy-policy.component.ts
+++ src/app/info-content/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [TranslocoModule],
+  imports: [TranslocoModule, FooterComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
 
+  closeTab() {
+    window.close();
+  }
 }
