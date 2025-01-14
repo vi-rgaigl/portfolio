9de91e88756016d947c6a6f3be867b5bd2cedbbf
Regina Gaigl <regina.gaigl@gmail.com>
2025-01-14 11:32:58 +0100

    toggle language

--- src/app/info-content/legal-notice/legal-notice.component.ts
+++ src/app/info-content/legal-notice/legal-notice.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
-import { TranslocoModule } from '@jsverse/transloco';
+import { CommonModule } from '@angular/common';
+import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [TranslocoModule ],
+  imports: [CommonModule, TranslocoModule ],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
+
+  currentLanguage: string = 'en';
+
+    constructor(public translocoService: TranslocoService) {}
+
+    toggleLanguage(lang: string) {
+      this.currentLanguage = lang;
+      this.translocoService.setActiveLang(this.currentLanguage);
+    }
 
   closeTab() {
     window.close();
--- src/app/info-content/privacy-policy/privacy-policy.component.ts
+++ src/app/info-content/privacy-policy/privacy-policy.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
-import { TranslocoModule } from '@jsverse/transloco';
+import { CommonModule } from '@angular/common';
+import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [TranslocoModule],
+  imports: [CommonModule,TranslocoModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
 
+  currentLanguage: string = 'en';
+
+    constructor(public translocoService: TranslocoService) {}
+
+    toggleLanguage(lang: string) {
+      this.currentLanguage = lang;
+      this.translocoService.setActiveLang(this.currentLanguage);
+    }
   closeTab() {
     window.close();
   }
